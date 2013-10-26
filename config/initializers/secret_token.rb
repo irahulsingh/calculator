d1152acb97ab532c3467f2ba5b27fc964837013a
Rahul Singh <rahul692000@gmail.com>
2013-10-27 01:07:44 +0530

    first commit,added jqueryui, draggable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalculatorApp::Application.config.secret_key_base = '732116e6f2a9f40b9bf50050a97e18ef5b5a4e9f4588d2f806aacc0d9363cdeab754f0ccfec2c00b539d2f484d62dc22542d2a2df22c11d13105def78e8b259e'
